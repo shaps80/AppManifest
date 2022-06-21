f26a9b8535bacaf59b6d06f7098ddcc9290e85ac
Shaps Benkau <shapsuk@me.com>
2022-06-21 20:51:21 +0100

    Renames demo project

--- Tests/AppManifestTests/EnvironmentTests.swift
+++ Tests/AppManifestTests/EnvironmentTests.swift
@@ -1,5 +1,5 @@
 import XCTest
-@testable import AppDescription
+@testable import AppManifest
 
 final class EnvironmentTests: XCTestCase {
 
--- Tests/AppManifestTests/SettingsTests.swift
+++ Tests/AppManifestTests/SettingsTests.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 import XCTest
-@testable import AppDescription
+@testable import AppManifest
 
 final class SettingsTests: XCTestCase {
 
