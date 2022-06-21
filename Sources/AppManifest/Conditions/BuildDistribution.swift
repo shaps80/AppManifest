6d4c439db97aa1b912179fb0a90ceef9997b29f1
Shaps Benkau <shapsuk@me.com>
2022-06-21 22:35:58 +0100

    Adding a print statement to try and understand what's happening

--- Sources/AppManifest/Conditions/BuildDistribution.swift
+++ Sources/AppManifest/Conditions/BuildDistribution.swift
@@ -71,6 +71,9 @@ public extension BuildDistribution where Self == Debugger {
 
 /// Represents a build being run in a Playground
 public struct Playground: BuildDistribution {
+    public init() {
+        print(Bundle.allBundles)
+    }
     /// Active is the build is being run via a Playground
     public var isActive: Bool {
         Bundle.allBundles
