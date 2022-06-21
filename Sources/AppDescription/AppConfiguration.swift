d013a9c5bc15c3d8a9ce7c5a7395d849583d847e
Shaps Benkau <shapsuk@me.com>
2022-06-21 17:32:25 +0100

    Improves header docs

--- Sources/AppDescription/AppConfiguration.swift
+++ Sources/AppDescription/AppConfiguration.swift
@@ -1,10 +1,25 @@
 @_exported import Foundation
 
-/// The configuration for an app
+/**
+ The configuration for an app, including the currently deployed environment.
+
+ To determine the preferred `Environment` in the current context, the following
+ rules are evaluated (priority top to bottom):
+
+ 1. Distribution AND configuration are both active
+ 2. Distribution only is active
+ 3. Configuration only is active
+ 4. First available Environment
+ */
 public struct AppConfiguration {
 
     /// Returns all configured environments
     public let environments: [Environment]
+
+    /// Returns all environments available in the current deployment
+    public var availableEnvironments: [Environment] {
+        environments.filter { $0.condition.isActive }
+    }
 
     /// Returns the preferred environment in the current context,
     /// taking into account active conditions
@@ -14,18 +29,6 @@ public struct AppConfiguration {
 
         let activeConfigurations = environments
             .filter { $0.condition.configuration.isActive }
-
-        /**
-         Find the most suitable environment using the following priority:
-
-         1. Distribution AND configuration are both active
-         2. Distribution only is active
-         3. Configuration only is active
-         4. First available Environment
-         */
-
-        // distribution: debugger AND configuration: RELEASE
-        // configuration: DEBUG
 
         return (activeDistributions + activeConfigurations).first
         ?? activeDistributions.first
@@ -40,7 +43,6 @@ public struct AppConfiguration {
         assert(!environments.isEmpty, "At least 1 valid environment configuration must be supplied")
         assert(environments.allSatisfy { !$0.name.isEmpty }, "1 or more Environments are missing a name. This is not allowed")
         assert(Set(environments.map { $0.name }).count == environments.map { $0.name }.count, "1 or more Environments have the same name. This is not allowed.")
-
         self.environments = environments
     }
 
